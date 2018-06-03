96c04f9a65f9ce10228bf0d09ceacd7ccf1ddd09
RogerOliveira <rogerolivierhp@gmail.com>
2018-06-03 17:44:39 -0300

    6_2

---
+++ arqsw_estruturais_comportamentais_a8/src/strategy_exercicio6_2/InsertionSort.java
@@ -0,0 +1,17 @@
+package strategy_exercicio6_2;
+
+public class InsertionSort implements Sort {
+
+	@Override
+	public int[] vetor(int[] v) {
+		int i, j, x;
+		for (j = 1; j < v.length; ++j) {
+			x = v[j];
+			for (i = j - 1; i >= 0 && v[i] > x; --i) {
+				v[i + 1] = v[i];
+			}
+			v[i + 1] = x;
+		}
+		return v;
+	}
+}
